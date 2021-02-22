66f5729b442a809073fca10cdbeb76e7e2187103
Lucas Fiorini <lucas.fiorini@avature.net>
2021-02-22 20:16:43 -0300

    Updates

---
+++ precache-manifest.1ae686d52249a795b527192dae3b45b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e6d7c4aa67247767d71a5ea90feb33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fbc1eef647409faaa781",
+    "url": "./static/css/main.31aa6b69.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "./static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc1eef647409faaa781",
+    "url": "./static/js/main.89fae2d3.chunk.js"
+  },
+  {
+    "revision": "3af9496b79045c74a07c",
+    "url": "./static/js/runtime-main.bddfdab7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
