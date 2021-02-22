98db1dcc86442605659393999f8bdb043ce3fb24
Lucas Fiorini <lucas.fiorini@avature.net>
2021-02-22 11:23:08 -0300

    new commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
+
 import './index.css';
 
 const logger = createLogger() 
